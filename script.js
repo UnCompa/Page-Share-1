7cc18d83cb867bbaade4d29364698788a36992a1
UnCompa <brandonddxd@gmail.com>
2024-09-01 16:28:19 -0500

    Pagina V0.01

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("saludo").addEventListener("click", () => {
+  alert("Hola Emily, se la quiere")
+})
+document.getElementById("mensaje").addEventListener("click", () => {
+  alert("Que sepas que amo como eres, nunca cambies, y que seamos amigos por mucho tiempo <3")
+})
